ace33d416b8a215bb960f1e43664c3b248e40e4b
fredtsui <yizhi.tsui@gmail.com>
2018-07-10 12:15:20 -0400

    init commit

---
+++ Bucket14/src/main/java/com/example/Buckets/Bucket14Application.java
@@ -0,0 +1,12 @@
+package com.example.Buckets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bucket14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bucket14Application.class, args);
+	}
+}
