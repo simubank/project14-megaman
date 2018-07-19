1e5899ff7abc69f1b4d5ae026d7664c2b22e7c29
fredtsui <yizhi.tsui@gmail.com>
2018-07-19 17:49:15 -0400

    divs

--- Bucket14/src/main/java/bankapp/User.java
+++ Bucket14/src/main/java/bankapp/User.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ public class User {
 	public String first_name;
 	public String last_name;
 	public Challenge list_of_challenges;
+	public ArrayList<Transaction> newTransactions;
 
 
 	public void login(int login_customer_id) {
